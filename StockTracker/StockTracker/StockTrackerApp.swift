f127bb5332e081a667c66a1b744562a4e2464ae0
lakshpurbey <laxmipurbey.vis@gmail.com>
2026-04-01 16:00:10 +0530

    Intial Project Setup

---
+++ StockTracker/StockTracker/StockTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockTrackerApp.swift
+//  StockTracker
+//
+//  Created by Laksh Purbey on 01/04/26.
+//
+
+import SwiftUI
+
+@main
+struct StockTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
